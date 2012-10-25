2a582fa56fa950a455621e4f130acca461e0e2f7
unknown <johaeuss@3DLABOR4.in.fhkn.de>
2012-10-25 11:57:36 +0200

    khn

--- website/SeaPal.php
+++ website/SeaPal.php
@@ -1,34 +1,9 @@
 <?php include("header.html");?>
 
 <body data-twttr-rendered="true">
-
-	<div id="header-wrapper">
-		<div id="header" class="container">
-			<div id="logo">
-				<h1><a name="top"><img src="./SeaPal_files/Icon-Small-50.png" alt="Icon-Small-50" width="50" height="50">SeaPal </a></h1>
-			</div>
-			<div id="menu">
-				<ul>
-					<li class="current_page_item">
-						<a href="./SeaPal_files/SeaPal.htm">Homepage</a>
-					</li>
-					<li>
-						<a href="http://www.seapal.de/userguide.php">User Guide</a>
-					</li>
-					<li>
-						<a href="http://www.seapal.de/screenshots.php">Screenshots</a>
-					</li>
-					<li>
-						<a href="http://www.seapal.de/about.php">About</a>
-					</li>
-					<li>
-						<a href="http://www.seapal.de/contact.php">Contact</a>
-					</li>
-				</ul>
-			</div>
-		</div>
-	</div>
-
+	
+	<?php include("content_header.html");?>
+	
 	<div id="wrapper">
 		<div id="page" class="container">
 			<div id="content">
---
+++ website/content_header.php
@@ -0,0 +1,26 @@
+<div id="header-wrapper">
+	<div id="header" class="container">
+		<div id="logo">
+			<h1><a name="top"><img src="./SeaPal_files/Icon-Small-50.png" alt="Icon-Small-50" width="50" height="50">SeaPal </a></h1>
+		</div>
+		<div id="menu">
+			<ul>
+				<li class="current_page_item">
+					<a href="./SeaPal_files/SeaPal.htm">Homepage</a>
+				</li>
+				<li>
+					<a href="http://www.seapal.de/userguide.php">User Guide</a>
+				</li>
+				<li>
+					<a href="http://www.seapal.de/screenshots.php">Screenshots</a>
+				</li>
+				<li>
+					<a href="http://www.seapal.de/about.php">About</a>
+				</li>
+				<li>
+					<a href="http://www.seapal.de/contact.php">Contact</a>
+				</li>
+			</ul>
+		</div>
+	</div>
+</div>
