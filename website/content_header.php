777d538305ac6ad29b89c55dae021be4452d8981
unknown <JojO@.(none)>
2012-11-06 21:12:02 +0100

    Website optimization

--- website/content_header.php
+++ website/content_header.php
@@ -1,25 +1,42 @@
+<?php
+function curPageName() {
+return substr($_SERVER["SCRIPT_NAME"],strrpos($_SERVER["SCRIPT_NAME"],"/")+1);
+}
+?>
 <div id="header-wrapper">
 	<div id="header" class="container">
 		<div id="logo">
-			<h1><a name="top"><img src="./SeaPal_files/Icon-Small-50.png" alt="Icon-Small-50" width="50" height="50">SeaPal </a></h1>
+			<h1><a name="top"><img src="./SeaPal_files/Icon-Small-50.png" alt="Icon-Small-50" width="50" height="50">SeaPal</a></h1>
 		</div>
 		<div id="menu">
 			<ul>
-				<li class="current_page_item">
-					<a href="./SeaPal_files/SeaPal.htm">Homepage</a>
-				</li>
-				<li>
-					<a href="http://www.seapal.de/userguide.php">User Guide</a>
-				</li>
-				<li>
-					<a href="http://www.seapal.de/screenshots.php">Screenshots</a>
-				</li>
-				<li>
-					<a href="http://www.seapal.de/about.php">About</a>
-				</li>
-				<li>
-					<a href="http://www.seapal.de/contact.php">Contact</a>
-				</li>
+				<?php
+				if(curPageName()=="SeaPal.php") {
+				echo "<li class=\"current_page_item\"><a href=\"SeaPal.php\">Homepage</a></li>";
+				} else {
+				echo "<li><a href=\"SeaPal.php\">Homepage</a></li>";
+				}
+				if(curPageName()=="userguide.php") {
+				echo "<li class=\"current_page_item\"><a href=\"userguide.php\">User Guide</a></li>";
+				} else {
+				echo "<li><a href=\"userguide.php\">User Guide</a></li>";
+				}
+				if(curPageName()=="screenshots.php") {
+				echo "<li class=\"current_page_item\"><a href=\"screenshots.php\">Screenshots</a></li>";
+				} else {
+				echo "<li><a href=\"screenshots.php\">Screenshots</a></li>";
+				}
+				if(curPageName()=="about.php") {
+				echo "<li class=\"current_page_item\"><a href=\"http://www.seapal.de/about.php\">About</a></li>";
+				} else {
+				echo "<li><a href=\"about.php\">About</a></li>";
+				}
+				if(curPageName()=="contact.php") {
+				echo "<li class=\"current_page_item\"><a href=\"contact.php\">Contact</a></li>";
+				} else {
+				echo "<li><a href=\"contact.php\">Contact</a></li>";
+				}
+				?>
 			</ul>
 		</div>
 	</div>
