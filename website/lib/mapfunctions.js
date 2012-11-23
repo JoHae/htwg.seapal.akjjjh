8cb6944aa73f19f4933ffa51b816ef3c3d43c1bb
unknown <johaeuss@.in.fhkn.de>
2012-11-23 13:45:34 +0100

    jh: map-feature: Markierung setzen

--- website/lib/mapfunctions.js
+++ website/lib/mapfunctions.js
@@ -22,11 +22,49 @@ function getPostionString(position) {
 	return LatVorkomma + "°" + LatMinutes + "'N " + LongVorkomma + "°" + LongMinutes + "'E";
 }
 
-function addstandardMark(position) {
+function setNewMarkerMenu(map) {
+	var topRight = map.getProjection().fromLatLngToPoint(map.getBounds().getNorthEast());
+	var bottomLeft = map.getProjection().fromLatLngToPoint(map.getBounds().getSouthWest());
+	var scale = Math.pow(2, map.getZoom());
+
+	var worldPoint = map.getProjection().fromLatLngToPoint(actualCrosshairMarker.getPosition());
+	var point = new google.maps.Point((worldPoint.x - bottomLeft.x) * scale, (worldPoint.y - topRight.y) * scale);
+
+	jQuery("#overlay").hide();
+	jQuery("#overlay").css({
+		left : (jQuery("#map_canvas").position().left + point.x),
+		top : (jQuery("#map_canvas").position().top + point.y)
+	});
+	jQuery("#overlay").show();
+
+	jQuery('#close').click(function(e) {
+		e.preventDefault();
+		jQuery("#overlay").hide();
+		actualCrosshairMarker.setVisible(false);
+		actualCrosshairMarker = null;
+	});
+}
+
+function addNewStandardMarker(map, position) {
 	var markerOptions = {
-		position : position,
+		position : actualCrosshairPosition,
 		map : map,
-		draggable : true
+		draggable : true,
+		icon : "",
+		labelContent : getPostionString(actualCrosshairPosition),
+		labelAnchor : new google.maps.Point(0, 0),
+		labelClass : "labels", // the CSS class for the label
+		labelStyle : {
+			opacity : 0.75
+		}
 	}
-	return new google.maps.Marker(markerOptions);
+	standardMarkers[0] = new MarkerWithLabel(markerOptions);
+
+	google.maps.event.addListener(standardMarkers[0], 'drag', function() {
+		standardMarkers[0].set("labelContent", getPostionString(standardMarkers[0].getPosition()));
+	});
+
+	google.maps.event.addListener(standardMarkers[0], 'dragend', function() {
+		standardMarkers[0].set("labelContent", getPostionString(standardMarkers[0].getPosition()));
+	});
 }
