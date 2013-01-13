9f8b672949d524cc05a5aec8fd3deafba2075f93
Janosch <janosch_not@hallo.de>
2013-01-13 22:39:24 +0100

    bugfix notes, play

--- app/server/play/seapal/public/app/lib/js/seapal.views.trips.js
+++ app/server/play/seapal/public/app/lib/js/seapal.views.trips.js
@@ -8,10 +8,10 @@ $(function() {
 	
 	seapalLogbookId = $.urlParam('logbookId');
 	
-	seapalListInit(getServiceURL('trips_get.php?logbookId=' + seapalLogbookId), getServiceURL('trip_delete.php?logbookId=' + seapalLogbookId), getServiceURL('trip_edit.php?logbookId=' + seapalLogbookId));
+	seapalListInit(getServiceURL('trips_get', 'logbookId', seapalLogbookId), getServiceURL('trip_delete', 'logbookId', seapalLogbookId), getServiceURL('trip_edit', 'logbookId', seapalLogbookId));
 	
 	// insert the navigation information
-	ajaxGet(getServiceURL('trips_navigationinfo_get.php?logbookId=' + seapalLogbookId), function(data) {
+	ajaxGet(getServiceURL('trips_navigationinfo_get', 'logbookId', seapalLogbookId), function(data) {
 		$(".seapal-logbookname").html(data.shipname);
 	});
 });
